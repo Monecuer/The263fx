bee5f1bf0ac4286094e1b951e7e9573a6054b8b0
Monecuer <anesuelsha4@gmail.com>
2025-07-11 00:40:44 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
-import WhatsappSupport from '../components/WhatsappSupport';
-import AIPopup from '../components/AIPopup';
+import Script from 'next/script';
+
+
 export const metadata = {
   title: 'The263fx - Trading Academy',
   description: 'Learn to trade forex with real strategies and tools.',
@@ -9,10 +10,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google AdSense Script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4437850977433689"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="bg-black text-white font-sans">
         {children}
-     <WhatsappSupport />
-        <AIPopup />
+       
       </body>
     </html>
   );
