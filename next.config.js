79042937535a1d35dfba973e07ac8ae6bc25969c
Elshaddai Anesu Mugugu <anesuelsha4@gmail.com>
2025-07-10 11:14:25 +0200

    Update message here

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['your-storage-domain.supabase.co'],
-  },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
-    NEXT_PUBLIC_CRYPTO_ADDR: process.env.NEXT_PUBLIC_CRYPTO_ADDR,
-    NEXT_PUBLIC_FLW_KEY: process.env.NEXT_PUBLIC_FLW_KEY,
-  }
+  },
 };
 
 module.exports = nextConfig;
