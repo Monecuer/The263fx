33a0c968343dfd7238ed1a3226c7d23a7f031b68
Monecuer <anesuelsha4@gmail.com>
2025-07-10 11:16:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['your-storage-domain.supabase.co'],
-  },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-   
-  }
+  },
 };
 
 module.exports = nextConfig;
+
