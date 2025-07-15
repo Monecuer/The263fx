c804f645b8966d286e6e2ed5c539289c61e7d3df
Monecuer <anesuelsha4@gmail.com>
2025-07-15 10:25:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,18 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 8s linear infinite',
         'scroll-horizontal': 'scrollX 25s linear infinite reverse',
         'bounce-slow': 'bounce 3s infinite',
+        'fade-in': 'fadeIn 1.5s ease-out forwards',
       },
       keyframes: {
         scrollX: {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(-100%)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
       },
     },
