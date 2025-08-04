ed1ef0106f32e50afa31bceb900c4e361abda3eb
MonecuerAnesu <socialmediametaplatform@gmail.com>
2025-08-04 11:01:38 +0200

    Update WhatsappSupport.jsx

--- src/app/components/WhatsappSupport.jsx
+++ src/app/components/WhatsappSupport.jsx
@@ -3,7 +3,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/+263775007225"
+      href="https://wa.me/+263775001909"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition duration-300"
